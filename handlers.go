6b5f918bb6dbd81b7d4b75b495c7ef3e75d261b6
chris92vr <garofolichristian@gmail.com>
2023-03-07 17:01:02 +0100

    Added removeBudgetByCookie function

--- handlers.go
+++ handlers.go
@@ -824,6 +824,76 @@ func getBudgetsByCookie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func removeBudgetByCookie(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
+	var budget models.Budget
+
+	var cookie string
+	cookie = r.URL.Query().Get("cookie")
+
+	err := validate.Struct(cookie)
+
+	if cookie == "" {
+		err := errors.New("cookie not found")
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else {
+		userSession, exists := sessions[cookie]
+		if !exists {
+			// If the session token is not present in session map, return an unauthorized error
+			w.WriteHeader(http.StatusUnauthorized)
+			
+		}
+		if userSession.isExpired() {
+			delete(sessions, cookie)
+			w.WriteHeader(http.StatusUnauthorized)
+		
+		}
+		
+	
+		var user models.User
+		err = userCollection.FindOne(context.TODO(), bson.M{"username": userSession.username}).Decode(&user)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			
+		}
+	
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			
+		} else {
+			fmt.Println("user id retrieved")
+		}
+
+		err := json.NewDecoder(r.Body).Decode(&budget)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = validate.Struct(budget)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		err = budgetCollection.FindOneAndDelete(context.TODO(), bson.M{"budget_id": budget.Budget_id, "user_id": user.User_id}).Decode(&budget)
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		} else {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(budget)
+		}
+	}
+
+}
 
 
 func deleteBudgetById(w http.ResponseWriter, r *http.Request) {
