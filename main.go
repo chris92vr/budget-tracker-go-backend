5800085c5057d3b823c644b411c0cbfbd752de6c
chris92vr <garofolichristian@gmail.com>
2023-03-08 18:55:49 +0100

    INtegrated github.com/rs/cors

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -12,25 +14,24 @@ func main() {
 	if port == "" {
 		port = "8000"
 	}
-	log.Printf("Listening on port %s", port)
-	log.Printf("Database_URL  %s", os.Getenv("MONGODB_URL"))
 	
-	http.HandleFunc("/signin", Login)
-	http.HandleFunc("/signup", Signup)
-	http.HandleFunc("/logout", Logout)
-	http.HandleFunc("/profile", MyProfile)
-	http.HandleFunc("/addbudget", addBudget)
-	http.HandleFunc("/addexpense", addExpense)
-	http.HandleFunc("/getbudgets", getAllBudget)
-	http.HandleFunc("/getexpenses", getExpensesById)
-	http.HandleFunc("/deleteexpense", deleteExpenseById)
-	http.HandleFunc("/deletebudget", deleteBudgetById)
-	http.HandleFunc("/getUserId", getUserIdByCookie)
-	http.HandleFunc("/totalBudget", totalMaxAndTotalAmountByUserId)
-	// start the server on port 8080
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/signin", Login)
+	mux.HandleFunc("/signup", Signup)
+	mux.HandleFunc("/logout", Logout)
+	mux.HandleFunc("/profile", MyProfile)
+	mux.HandleFunc("/addbudget", addBudget)
+	mux.HandleFunc("/addexpense", addExpense)
+	mux.HandleFunc("/getbudgets", getAllBudget)
+	mux.HandleFunc("/getexpenses", getExpensesById)
+	mux.HandleFunc("/deleteexpense", deleteExpenseById)
+	mux.HandleFunc("/deletebudget", deleteBudgetById)
+	mux.HandleFunc("/getBudgetsByCookie", getBudgetsByCookie)
+	mux.HandleFunc("/totalBudget", totalMaxAndTotalAmountByUserId)
+	mux.HandleFunc("/updateBudget", addBudgetByCookies)
 
-	//http.ListenAndServe(":"+port, handler)
-	
+
+	handler := cors.Default().Handler(mux)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
 
